7d6ad9671f853c5a885d83249b200b7852e993d5
Armando Maynez <amaynez23@hotmail.com>
2021-02-11 00:04:02 -0600

    First working prototype

--- Neural_Network.py
+++ Neural_Network.py
@@ -1,9 +1,7 @@
 import numpy as np
 
-
 def sigmoid(x):
     return np.exp(-np.logaddexp(0, -x))
-
 
 class NeuralNetwork:
     def __init__(self, inputs, hidden, outputs):
@@ -24,28 +22,35 @@ class NeuralNetwork:
         # create weights for output layer (defined based on number of outputs)
         self.weights.append([np.random.uniform(-1, 1, size=(self.outputs, self.hidden[-1],))])
 
-        # create bias array (one bias per each hidden layer plus one for the output)
-        self.bias = np.random.uniform(-1, 1, size=(self.hidden.shape[0] + 1))
+        # create a bias matrix
+        self.bias = []
+        for idx, hidden_col in enumerate(self.hidden):
+            self.bias.append(np.random.uniform(-1, 1, size=(hidden_col, 1)))
+        self.bias.append(np.random.uniform(-1, 1, size=(self.outputs, 1)))
 
     def forward_propagation(self, input_values):
-        # create results matrix based on the maximum number of cells in the hidden layers
+        # create hidden results matrix based on the maximum number of cells in the hidden layers
         hidden_results = np.zeros((np.max(self.hidden), self.hidden.shape[0]))
-
-        # calculate results for the first hidden layer (depends on the inputs)
+        # prepare the input values
         input_values = np.array(input_values)[np.newaxis]
         input_values = input_values.T
-        hidden_results[:self.hidden[0], :1] =\
-            sigmoid(np.matmul(self.weights[0], input_values) +
-                    self.bias[0])
+
+        # calculate results for the first hidden layer (depends on the inputs)
+        hidden_results[:self.hidden[0] + 1, :1] = \
+            sigmoid(np.matmul(self.weights[0], input_values) + self.bias[0])
 
         # calculate results for subsequent hidden layers if any (depending on the previous layer)
         if self.hidden.ndim > 0:
             for idx, hidden_cells in enumerate(self.hidden[1:]):
-                hidden_results[:self.hidden[idx+1], idx+1:idx+2] =\
-                    sigmoid(np.matmul(self.weights[idx+1], hidden_results[:self.hidden[idx], idx:idx+1]) +
-                            self.bias[idx+1])
+                hidden_results[:self.hidden[idx + 1], idx + 1:idx + 2] = \
+                    sigmoid(np.matmul(self.weights[idx + 1],
+                                      hidden_results[:self.hidden[idx],
+                                      idx:idx + 1]
+                                      )
+                            + self.bias[idx + 1])
 
         # calculate final result and return
-        return sigmoid(np.matmul(self.weights[-1], hidden_results[:self.hidden[-1], -1:]) + self.bias[-1])
-
-
+        return sigmoid(np.matmul(self.weights[-1],
+                                 hidden_results[:self.hidden[-1], -1:]
+                                 )
+                       + self.bias[-1])
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import numpy as np
 import Neural_Network as nn
 
-NN = nn.NeuralNetwork(728, [16, 16], 8)
+inputs = 5
+hidden_layers = [10, 2, 2]
+outputs = 1
 
-inputs = np.arange(728)
+NN = nn.NeuralNetwork(inputs, hidden_layers, outputs)
 
-print(NN.forward_propagation(inputs), '\n')
+inputs_fp = np.arange(inputs)
+
+print(NN.forward_propagation(inputs_fp), '\n')
 # for weights in NN.weights:
 #     print(weights, '\n')
 # print(NN.bias, '\n')
